4fe443752be9b13634b978e9ebc1855b249bfda0
ravis-teamopine <ravis@teamopine.com>
2024-09-11 09:56:50 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, India" +
+                "" +
+                "" +
+                "" +
+                "!");
+
+        }
+        
+        
+
+}
+}
+ 
+
